e2bfa97bc5f9e79e0956f715dc054509d8335c07
K4rakara <jack@insertdomain.name>
2020-06-12 14:56:29 -0400

    Logic change

--- index.ts
+++ index.ts
@@ -30,62 +30,56 @@ const replacements: { [key: string]: string; } =
 
 export function clml(strings: TemplateStringsArray, ...args: string[]): string
 {
-	let cloned: string[] = Array.from(strings);
+	let output: string = '';
+	strings.forEach((string: string, i: number): void =>
+	{
+		output += string;
+		output += args[i] || '';
+	});
 
 	// Do all replacements.
 	Object.keys(replacements).forEach((key: string): void =>
 	{
 		const val: string = replacements[key];
-		cloned = cloned.map((string: string): string => string.replace(new RegExp(`<${key}>`, 'gmi'), val));
+		output = output.replace(new RegExp(`<${key}>`, 'gmi'), val);
 	});
 
 	// Do all special mutations.
-	cloned = cloned.map((string: string): string =>
-	{
-		// 255-color tag
-		string = string.replace(/<255(?:-bg)? (?:\d\d?\d?)>/gmi, (substring: string): string =>
-			`\u001B[${(substring.includes('-bg')) ? '48' : '38'};5;${substring.match(/(?<=<255(?:-bg)? )\d+(?=>)/gmi)?.toString()}m`);
-		
-		// rgb-color tag
-		string = string.replace(/<rgb(?:-bg)? (?:\d\d?\d?) (?:\d\d?\d?) (?:\d\d?\d?)>/gmi, (substring: string): string =>
-			`\u001B[${(substring.includes('-bg')) ? '48' : '38'};2;${
-					substring.match(/(?<=<rgb(?:-bg)? )(?:\d\d?\d?)/gmi)?.toString()
-				};${
-					substring.match(/(?<=<rgb(?:-bg)? (?:\d\d?\d?) )(?:\d\d?\d?)/gmi)?.toString()
-				};${
-					substring.match(/(?<=<rgb(?:-bg)? (?:\d\d?\d?) (?:\d\d?\d?) )(?:\d\d?\d?)/gmi)?.toString()
-				}m`);
-		
-		// Cursor movement tags
-		string = string.replace(/<to \d+ \d+>/gmi, (substring: string): string =>
-			`\u001B[${
-				substring.match(/(?<=<to )\d+/gmi)?.toString()
+
+	// 255-color tag
+	output = output.replace(/<255(?:-bg)? (?:\d\d?\d?)>/gmi, (substring: string): string =>
+		`\u001B[${(substring.includes('-bg')) ? '48' : '38'};5;${substring.match(/(?<=<255(?:-bg)? )\d+(?=>)/gmi)?.toString()}m`);
+	
+	// rgb-color tag
+	output = output.replace(/<rgb(?:-bg)? (?:\d\d?\d?) (?:\d\d?\d?) (?:\d\d?\d?)>/gmi, (substring: string): string =>
+		`\u001B[${(substring.includes('-bg')) ? '48' : '38'};2;${
+				substring.match(/(?<=<rgb(?:-bg)? )(?:\d\d?\d?)/gmi)?.toString()
 			};${
-				substring.match(/(?<=<to \d+ )\d+/gmi)?.toString()
-			}H`);
-		string = string.replace(/<up \d+>/gmi, (substring: string): string =>
-			`\u001B[${substring.match(/(?<=<up )\d+/gmi)?.toString()}A`);
-		string = string.replace(/<down \d+>/gmi, (substring: string): string =>
-			`\u001B[${substring.match(/(?<=<down )\d+/gmi)?.toString()}B`);
-		string = string.replace(/<forward \d+>/gmi, (substring: string): string =>
-			`\u001B[${substring.match(/(?<=<forward )\d+/gmi)?.toString()}C`);
-		string = string.replace(/<backward \d+>/gmi, (substring: string): string =>
-			`\u001B[${substring.match(/(?<=<backward )\d+/gmi)?.toString()}D`);
+				substring.match(/(?<=<rgb(?:-bg)? (?:\d\d?\d?) )(?:\d\d?\d?)/gmi)?.toString()
+			};${
+				substring.match(/(?<=<rgb(?:-bg)? (?:\d\d?\d?) (?:\d\d?\d?) )(?:\d\d?\d?)/gmi)?.toString()
+			}m`);
+	
+	// Cursor movement tags
+	output = output.replace(/<to \d+ \d+>/gmi, (substring: string): string =>
+		`\u001B[${
+			substring.match(/(?<=<to )\d+/gmi)?.toString()
+		};${
+			substring.match(/(?<=<to \d+ )\d+/gmi)?.toString()
+		}H`);
+	output = output.replace(/<up \d+>/gmi, (substring: string): string =>
+		`\u001B[${substring.match(/(?<=<up )\d+/gmi)?.toString()}A`);
+	output = output.replace(/<down \d+>/gmi, (substring: string): string =>
+		`\u001B[${substring.match(/(?<=<down )\d+/gmi)?.toString()}B`);
+	output = output.replace(/<forward \d+>/gmi, (substring: string): string =>
+		`\u001B[${substring.match(/(?<=<forward )\d+/gmi)?.toString()}C`);
+	output = output.replace(/<backward \d+>/gmi, (substring: string): string =>
+		`\u001B[${substring.match(/(?<=<backward )\d+/gmi)?.toString()}D`);
 
-		// Emoji tags
-		string = string.replace(/<:[a-z0-9\-]+:>/gmi, (substring: string): string =>
-			emoji.emojify(substring.substring(1, substring.length - 1).toLowerCase()));
+	// Emoji tags
+	output = output.replace(/<:[a-z0-9\-]+:>/gmi, (substring: string): string =>
+		emoji.emojify(substring.substring(1, substring.length - 1).toLowerCase()));
 
-		return string;
-	});
-
-	// Join the final output.
-	let output: string = '';
-	cloned.forEach((string: string, i: number): void =>
-	{
-		output += string;
-		output += args[i] || '';
-	});
 
 	return output;
 }
